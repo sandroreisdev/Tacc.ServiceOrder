7b96cc928f40982578d6020a2fdb90bf7730fe69
sandroreisdev <sandroreisdev@gmail.com>
2026-02-23 18:36:22 -0300

    feat: add BaseEntity to Domain layer

---
+++ src/Tacc.ServiceOrder.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Tacc.ServiceOrder.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
+    }
+}
