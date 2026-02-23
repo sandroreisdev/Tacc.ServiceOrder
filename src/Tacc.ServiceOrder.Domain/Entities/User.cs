f39c03e03794faf590cd75898f47ea01c520faf5
sandroreisdev <sandroreisdev@gmail.com>
2026-02-23 19:20:35 -0300

    feat(domain): add User entity with nullable property handling

---
+++ src/Tacc.ServiceOrder.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Tacc.ServiceOrder.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? IpAdress { get; set; }
+        public string? Phone { get; set; }
+        public string? ProfileImage { get; set; }
+    }
+}
