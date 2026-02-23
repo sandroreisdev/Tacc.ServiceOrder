cef53fd9fb4155c5205dcd85a83febd39ccb272b
sandroreisdev <sandroreisdev@gmail.com>
2026-02-23 18:53:53 -0300

    feat(domain): refine Manufacturing entity and setup One-to-Many relationship with Product

---
+++ src/Tacc.ServiceOrder.Domain/Entities/Manufacturing.cs
@@ -0,0 +1,10 @@
+﻿namespace Tacc.ServiceOrder.Domain.Entities
+{
+    public class Manufacturing : BaseEntity
+    {       
+        public string Name { get; set; } = string.Empty;
+        public string DocumentNumber { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public IEnumerable<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ src/Tacc.ServiceOrder.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Tacc.ServiceOrder.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Manufacturing Manufature { get; set; } = null;
+        public int ManufactureId { get; set; }
+    }
+}
