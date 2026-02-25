a8f30e98f6c86d07a8fb454f0a98b8f5684a7711
sandroreisdev <sandroreisdev@gmail.com>
2026-02-25 01:15:06 -0300

    chore(domain): add service order diagram file

--- src/Tacc.ServiceOrder.Domain/Entities/User.cs
+++ src/Tacc.ServiceOrder.Domain/Entities/User.cs
@@ -2,6 +2,7 @@
 {
     public class User : BaseEntity
     {
+        //altere de Name para FirstName e depois criar outra LastName bem melhor para mostrar em relatórios de usuário logado
         public string Name { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
---
+++ src/Tacc.ServiceOrder.Infrastructure/Context/TaccContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tacc.ServiceOrder.Infrastructure.Context
+{
+    public class TaccContext : DbContext
+    {
+
+    }
+}
